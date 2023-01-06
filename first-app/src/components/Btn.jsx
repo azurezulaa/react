4f6844abeefacecbd0c2d20b40020c83707590a9
Evening <evening@mstars-lab3-05s-iMac.local>
2023-01-06 19:02:52 +0800

    1.6

---
+++ first-app/src/components/Btn.jsx
@@ -0,0 +1,9 @@
+const Btn = (props) => {
+  const angilah = () => {};
+  return (
+    <button className="font18" onClick={angilah}>
+      {props.btn}
+    </button>
+  );
+};
+export default Btn;
--- first-app/src/components/Content/Content.jsx
+++ first-app/src/components/Content/Content.jsx
@@ -1,3 +1,5 @@
+import Btn from "../Btn";
+import Card from "../Card";
 const menus = [
   "All",
   "UI Design",
@@ -7,21 +9,70 @@ const menus = [
   "Tutorials",
   "News",
 ];
-const Menu = (props) => (
-  <a className="content-tag" href="#">
-    {props.btn}
-  </a>
-);
+const blogs = [
+  {
+    category: "UI",
+    title: "This way is wrong about UI Design.",
+    text: "A quick guide to assisting users in the challenging steps from learning about your podcast on the web.  A quick guide to assisting users in the challenging steps from learning about your podcast on the web.",
+    image: "/images/content1.png",
+    date: "NOV 23 2020",
+    link: "#",
+  },
+  {
+    category: "UX",
+    title: "This way is wrong about UX Design.",
+    text: "A quick guide to assisting users in the challenging steps from learning about your podcast on the web.  A quick guide to assisting users in the challenging steps from learning about your podcast on the web.",
+    image: "/images/content2.png",
+    date: "NOV 23 2020",
+    link: "#",
+  },
+  {
+    category: "UI",
+    title: "This way is wrong about UI Design.",
+    text: "A quick guide to assisting users in the challenging steps from learning about your podcast on the web.  A quick guide to assisting users in the challenging steps from learning about your podcast on the web.",
+    image: "/images/content1.png",
+    date: "NOV 23 2020",
+    link: "#",
+  },
+  {
+    category: "UI",
+    title: "This way is wrong about UI Design.",
+    text: "A quick guide to assisting users in the challenging steps from learning about your podcast on the web.  A quick guide to assisting users in the challenging steps from learning about your podcast on the web.",
+    image: "/images/content2.png",
+    date: "NOV 23 2020",
+    link: "#",
+  },
+  {
+    category: "UX",
+    title: "This way is wrong about UX Design.",
+    text: "A quick guide to assisting users in the challenging steps from learning about your podcast on the web.  A quick guide to assisting users in the challenging steps from learning about your podcast on the web.",
+    image: "/images/content1.png",
+    date: "NOV 23 2020",
+    link: "#",
+  },
+  {
+    category: "UI",
+    title: "This way is wrong about UI Design.",
+    text: "A quick guide to assisting users in the challenging steps from learning about your podcast on the web.  A quick guide to assisting users in the challenging steps from learning about your podcast on the web.",
+    image: "/images/content2.png",
+    date: "NOV 23 2020",
+    link: "#",
+  },
+];
 
 const Content = () => {
   return (
     <div className="container">
-      <div>
+      <div className="content-menu">
         {menus.map((menu) => {
-          return <Menu btn={menu} />;
+          return <Btn btn={menu} />;
         })}
       </div>
-      <div></div>
+      <div className="grid">
+        {blogs.map((blog) => {
+          return <Card card={blog} />;
+        })}
+      </div>
     </div>
   );
 };
